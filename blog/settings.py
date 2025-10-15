804bfe0b916133a9c5463e86b2ee412430d2c1c1
Thirumal R <thirumal270997@gmail.com>
2025-10-15 20:01:37 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'True'
@@ -174,7 +174,7 @@ LOGGING = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
